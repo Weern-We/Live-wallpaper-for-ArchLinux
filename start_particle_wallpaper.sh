86aaa12b067b1d05e6722d9d5e351f5afd77dabc
weern <cheripkoaleksey@gmail.com>
2026-07-27 18:39:56 +0300

    Update start_particle_wallpaper.sh

--- start_particle_wallpaper.sh
+++ start_particle_wallpaper.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-xwinwrap -ov -ni -fs -nf -b -- python /home/silver/particle_wallpaper.py
+cd 
+git clone https://github.com/knifeyy/Live-wallpaper-for-ArchLinux.git
+pip3 install pygame --break-system-packages
+pamac build xwinwrap-git
+xwinwrap -ov -ni -fs -nf -b -- python $PWD/particle_wallpaper.py
